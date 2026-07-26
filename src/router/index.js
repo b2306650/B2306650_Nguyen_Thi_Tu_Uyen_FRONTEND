543f97240fe346c03cbdc41c93905b4ba404fea3
Nguyen Thi Tu Uyen <uyenb2306650@student.ctu.edu.vn>
2026-07-26 11:38:00 +0700

    Cai dat chuc nang them moi lien he

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,11 @@ const routes = [
         component: () => import("@/views/ContactEdit.vue"),
         props: true
     },
+     {
+        path: "/contacts/add",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+    },
 ];
 
 const router = createRouter({
